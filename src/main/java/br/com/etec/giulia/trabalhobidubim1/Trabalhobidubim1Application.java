f3593d069c6dfb46a7f0ab0d2e43bf493a83d973
giuliabferreira <giuliabferreira@hotmail.com>
2023-03-30 08:51:33 -0300

    Initial commit

---
+++ src/main/java/br/com/etec/giulia/trabalhobidubim1/Trabalhobidubim1Application.java
@@ -0,0 +1,13 @@
+package br.com.etec.giulia.trabalhobidubim1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalhobidubim1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalhobidubim1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/etec/giulia/trabalhobidubim1/Trabalhobidubim1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etec.giulia.trabalhobidubim1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalhobidubim1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
